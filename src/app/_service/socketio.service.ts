7eeddc0cf2da4ef743e7d7ab7db369cab77617d9
mohamedmadian10 <mohamed.madian183@gmail.com>
2020-02-15 14:07:48 +0200

    first commit

--- src/app/_service/socketio.service.ts
+++ src/app/_service/socketio.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 
@@ -8,5 +9,13 @@ export class SocketioService {
   socket;
 
   constructor() { }
-  set
+  setupSocketConnection(){
+    this.socket = io(environment.SOCKET_ENDPOINT);
+    //emit event
+    this.socket.emit('my message','saba7 elfol everybody grom angular app');
+    //add an listener for my broadcast event on our Angular app
+    this.socket.on('my broadcast',(data:string)=>{
+      console.log(data)
+    })
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SocketioService } from './_service/socketio.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'socketio-angular';
+  constructor(private socketService:SocketioService){}
+  ngOnInit() {
+    this.socketService.setupSocketConnection();
+    
+    
+  }
 }
